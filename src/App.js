f24de6b98c28bdffde95b245cf41b021f63abe69
Vera <58885611+barcelo2@users.noreply.github.com>
2021-05-10 09:18:25 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Book from './book';
+import data from './data.json';
+
+function App(){
+  const books=data.map(item=><Book key={item.id} title={item.title} author={item.author} />)
+   
+  return(
+ <div>
+     {books}
+ </div>
+
+  );
+
+}
+
+export default App;
